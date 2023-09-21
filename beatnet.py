0df7800b93197cc7215d292bad8add20b28902ad
Winton Davies <wdavies@cs.stanford.edu>
2023-09-20 21:50:49 -0700

    fixed indents

--- beatnet.py
+++ beatnet.py
@@ -18,18 +18,17 @@ def audiosegment_to_wav(audiosegment):
 
 
 def beatnet(params):
-    print("HERE")
-    full = params.get("full", None)
-    drums = params.get("drums", None)
-    drumoffset = params.get("drumoffset", None)
-    beatnet_selector = params.get("beatnet_selector ", 1)
-    bars = params.get("bars", None) 
+  full = params.get("full", None)
+  drums = params.get("drums", None)
+  drumoffset = params.get("drumoffset", None)
+  beatnet_selector = params.get("beatnet_selector ", 1)
+  bars = params.get("bars", None) 
  
-    if beatnet_selector == "librosa_drums" or  beatnet_selector == "librosa_full":
-      if beatnet_selector == "librosa_full":
-        y, sr = librosa.load(full, offset=0.0)
-      if beatnet_selector == "librosa_drums":
-        y, sr = librosa.load(drums, offset=0.0)
+  if beatnet_selector == "librosa_drums" or  beatnet_selector == "librosa_full":
+    if beatnet_selector == "librosa_full":
+      y, sr = librosa.load(full, offset=0.0)
+    if beatnet_selector == "librosa_drums":
+      y, sr = librosa.load(drums, offset=0.0)
       
     unused_tempo, beats = librosa.beat.beat_track(y=y, sr=sr, trim=False)
     beats = librosa.util.fix_frames(beats, x_min=0)
@@ -37,40 +36,40 @@ def beatnet(params):
     return [{'start': x} for x in beat_times]
     
     
-    estimator = BeatNet(beatnet_selector, mode='offline', inference_model='DBN', plot=[], thread=False)
+  estimator = BeatNet(beatnet_selector, mode='offline', inference_model='DBN', plot=[], thread=False)
     
-    drumoffset_msecs = 0
-    if drumoffset and drums:
-      drums_audiosegment = AudioSegment.from_file(drums)
-      drumoffset_msecs = silence.detect_leading_silence(drums_audiosegment, silence_threshold=-45, chunk_size=10)
+  drumoffset_msecs = 0
+  if drumoffset and drums:
+    drums_audiosegment = AudioSegment.from_file(drums)
+    drumoffset_msecs = silence.detect_leading_silence(drums_audiosegment, silence_threshold=-45, chunk_size=10)
 
-    if drums:
-      drums_audiosegment = AudioSegment.from_file(drums)
-      trimmed_drums_audiosegment = drums_audiosegment[drumoffset_msecs:]
-      trimmed_drums_mono_audiosegment = trimmed_drums_audiosegment.split_to_mono()
-      trimmed_drums_mono_audiosegment = trimmed_drums_mono_audiosegment[0]
-      trimmed_drums_mono_audiosegment = trimmed_drums_mono_audiosegment.set_frame_rate(22050)
-      trimmed_drums_mono_wav, _ = audiosegment_to_wav(trimmed_drums_mono_audiosegment)
-      trimmed_drums_mono_wav = trimmed_drums_mono_wav.astype('float32')
+  if drums:
+    drums_audiosegment = AudioSegment.from_file(drums)
+    trimmed_drums_audiosegment = drums_audiosegment[drumoffset_msecs:]
+    trimmed_drums_mono_audiosegment = trimmed_drums_audiosegment.split_to_mono()
+    trimmed_drums_mono_audiosegment = trimmed_drums_mono_audiosegment[0]
+    trimmed_drums_mono_audiosegment = trimmed_drums_mono_audiosegment.set_frame_rate(22050)
+    trimmed_drums_mono_wav, _ = audiosegment_to_wav(trimmed_drums_mono_audiosegment)
+    trimmed_drums_mono_wav = trimmed_drums_mono_wav.astype('float32')
     
-    if full:
-      full_audiosegment = AudioSegment.from_file(full)
-      trimmed_full_audiosegment = full_audiosegment[drumoffset_msecs:]
-      trimmed_full_mono_audiosegment = trimmed_full_audiosegment.split_to_mono()
-      trimmed_full_mono_audiosegment = trimmed_full_mono_audiosegment[0]
-      trimmed_full_mono_audiosegment = trimmed_full_mono_audiosegment.set_frame_rate(22050)
-      trimmed_full_mono_wav, _ = audiosegment_to_wav(trimmed_full_mono_audiosegment)
-      trimmed_full_mono_wav = trimmed_full_mono_wav.astype('float32')
+  if full:
+    full_audiosegment = AudioSegment.from_file(full)
+    trimmed_full_audiosegment = full_audiosegment[drumoffset_msecs:]
+    trimmed_full_mono_audiosegment = trimmed_full_audiosegment.split_to_mono()
+    trimmed_full_mono_audiosegment = trimmed_full_mono_audiosegment[0]
+    trimmed_full_mono_audiosegment = trimmed_full_mono_audiosegment.set_frame_rate(22050)
+    trimmed_full_mono_wav, _ = audiosegment_to_wav(trimmed_full_mono_audiosegment)
+    trimmed_full_mono_wav = trimmed_full_mono_wav.astype('float32')
 
-    if full and not drums:
-      bn_data = estimator.process(trimmed_full_mono_wav)
-    if  drums and not full:
-      bn_data = estimator.process(trimmed_drums_mono_wav)
-    if  full and drums: 
-      unused_bn_data = estimator.process(trimmed_full_mono_wav)
-      bn_data = estimator.process(trimmed_drums_mono_wav)
+  if full and not drums:
+    bn_data = estimator.process(trimmed_full_mono_wav)
+  if  drums and not full:
+    bn_data = estimator.process(trimmed_drums_mono_wav)
+  if  full and drums: 
+    unused_bn_data = estimator.process(trimmed_full_mono_wav)
+    bn_data = estimator.process(trimmed_drums_mono_wav)
 
-    if not bars:
-      return [{'start': x[0]+(drumoffset_msec/1000)} for x in bn_data ]
-    else:
-      return [{'start': x[0]+(drumoffset_msec/1000)} for x in bn_data if x[1] == 1.0]
+  if not bars:
+    return [{'start': x[0]+(drumoffset_msec/1000)} for x in bn_data ]
+  else:
+    return [{'start': x[0]+(drumoffset_msec/1000)} for x in bn_data if x[1] == 1.0]
